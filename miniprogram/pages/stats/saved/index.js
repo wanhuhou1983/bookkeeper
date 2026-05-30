966f8af08ebcd632b0dbb63cca0faed80e5a0d80
wanhuhou1983 <wanhuhou1983@users.noreply.github.com>
2026-05-30 21:24:13 +0800

    fix: saved-searches page - remove async/await, fix bind:tap→bindtap, english labels

--- miniprogram/pages/stats/saved/index.js
+++ miniprogram/pages/stats/saved/index.js
@@ -10,83 +10,82 @@ Page({
     renameName: ''
   },
 
-  onShow() {
+  onShow: function() {
     this.loadList()
   },
 
-  async loadList() {
+  loadList: function() {
+    var that = this
     this.setData({ loading: true })
-    try {
-      const data = await get('/saved-searches')
-      const list = Array.isArray(data) ? data : (data.list || [])
-      this.setData({ list, loading: false })
-    } catch (err) {
-      this.setData({ loading: false })
-    }
+    get('/saved-searches').then(function(data) {
+      var list = Array.isArray(data) ? data : (data.list || [])
+      that.setData({ list: list, loading: false })
+    }).catch(function() {
+      that.setData({ loading: false })
+    })
   },
 
-  goDetail(e) {
-    const id = e.currentTarget.dataset.id
+  goDetail: function(e) {
+    var id = e.currentTarget.dataset.id
     wx.navigateTo({ url: '/pages/stats/detail/index?id=' + id + '&type=saved' })
   },
 
-  onLongPress(e) {
-    const { id, name } = e.currentTarget.dataset
+  onLongPress: function(e) {
+    var that = this
+    var id = e.currentTarget.dataset.id
+    var name = e.currentTarget.dataset.name
     wx.showActionSheet({
-      itemList: ['重命名', '删除'],
-      success: (res) => {
+      itemList: ['Rename', 'Delete'],
+      success: function(res) {
         if (res.tapIndex === 0) {
-          this.setData({ showRename: true, renameId: id, renameName: name })
+          that.setData({ showRename: true, renameId: id, renameName: name })
         } else if (res.tapIndex === 1) {
-          this.onDelete(id, name)
+          that.onDelete(id, name)
         }
       }
     })
   },
 
-  onDelete(id, name) {
+  onDelete: function(id, name) {
+    var that = this
     wx.showModal({
-      title: '确认删除',
-      content: `确定删除统计项「${name}」吗？`,
-      success: async (res) => {
+      title: 'Confirm delete',
+      content: 'Delete "' + name + '"?',
+      success: function(res) {
         if (res.confirm) {
-          try {
-            await del('/saved-searches/' + id)
-            wx.showToast({ title: '删除成功', icon: 'success' })
-            this.loadList()
-          } catch (err) {
-            // handled
-          }
+          del('/saved-searches/' + id).then(function() {
+            wx.showToast({ title: 'Deleted', icon: 'success' })
+            that.loadList()
+          }).catch(function() {})
         }
       }
     })
   },
 
-  onRenameInput(e) {
+  onRenameInput: function(e) {
     this.setData({ renameName: e.detail })
   },
 
-  async onRenameConfirm() {
-    const name = this.data.renameName.trim()
+  onRenameConfirm: function() {
+    var that = this
+    var name = this.data.renameName.trim()
     if (!name) {
-      wx.showToast({ title: '请输入名称', icon: 'none' })
+      wx.showToast({ title: 'Enter name', icon: 'none' })
       return
     }
-    try {
-      await put('/saved-searches/' + this.data.renameId, { name })
-      this.setData({ showRename: false })
-      wx.showToast({ title: '重命名成功', icon: 'success' })
-      this.loadList()
-    } catch (err) {
-      // handled
-    }
+    put('/saved-searches/' + this.data.renameId, { name: name }).then(function() {
+      that.setData({ showRename: false })
+      wx.showToast({ title: 'Renamed', icon: 'success' })
+      that.loadList()
+    }).catch(function() {})
   },
 
-  onRenameCancel() {
+  onRenameCancel: function() {
     this.setData({ showRename: false })
   },
 
-  onPullDownRefresh() {
-    this.loadList().then(() => wx.stopPullDownRefresh())
+  onPullDownRefresh: function() {
+    var that = this
+    this.loadList().then(function() { wx.stopPullDownRefresh() })
   }
 })
